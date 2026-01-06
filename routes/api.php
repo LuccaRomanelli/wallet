509396607c407391c0c62564a24ccb9925ee832c
Lucca Romanelli <ldpromanelli@gmail.com>
2026-01-06 19:32:05 -0300

    feat/get-balance

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\GetUserBalanceController;
 use App\Http\Controllers\Api\StoreStoreController;
 use App\Http\Controllers\Api\StoreUserController;
 use App\Http\Controllers\Api\TransferController;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/transfer', TransferController::class);
 Route::post('/users', StoreUserController::class);
 Route::post('/stores', StoreStoreController::class);
+Route::get('/users/{id}/balance', GetUserBalanceController::class);
