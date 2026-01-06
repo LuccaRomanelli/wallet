dc846c40fd2f9ba813fa3d491cf30ea7b0de0e66
Lucca Romanelli <ldpromanelli@gmail.com>
2026-01-06 19:32:05 -0300

    feat/get-balance

---
+++ app/Http/Controllers/Api/GetUserBalanceController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Exceptions\Transfer\UserNotFoundException;
+use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Services\WalletBalanceService;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class GetUserBalanceController extends Controller
+{
+    public function __construct(
+        private UserRepositoryInterface $userRepository,
+        private WalletBalanceService $walletBalanceService
+    ) {}
+
+    public function __invoke(int $id): JsonResponse
+    {
+        $user = $this->userRepository->find($id);
+
+        if ($user === null) {
+            throw new UserNotFoundException('id');
+        }
+
+        $balance = $this->walletBalanceService->getBalance($id);
+
+        return response()->json([
+            'data' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'document' => $user->document,
+                'user_type' => $user->user_type->value,
+                'balance' => $balance->toDecimal(),
+            ],
+        ], Response::HTTP_OK);
+    }
+}
